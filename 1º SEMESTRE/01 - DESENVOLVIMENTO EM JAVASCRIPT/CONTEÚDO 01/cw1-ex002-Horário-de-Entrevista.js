50195431d49ddff7eed6b2d344754ce678660a45
Augusto Rock <augusto.roque89@gmail.com>
2024-01-30 21:58:14 -0300

    ESTUDO DE CASO + DOM
    Correção do Estudo de Caso
    Introdução ao DOM

--- 1º SEMESTRE/01 - DESENVOLVIMENTO EM JAVASCRIPT/CONTEÚDO 01/cw1-ex002-Horário-de-Entrevista.js
+++ 1º SEMESTRE/01 - DESENVOLVIMENTO EM JAVASCRIPT/CONTEÚDO 01/cw1-ex002-Horário-de-Entrevista.js
@@ -2,7 +2,6 @@ var inico = 9
 var final = 17
 var tempoEntrevista = 15
 var quantEntrevistados = 0
-
 var listaEntrevistado = [
     "José da Silva",
     "Antônio de Sá",
@@ -37,19 +36,58 @@ for( i = inico; i < final; i++){
         continue;
     }
         for(j=0;j<60;j=j+tempoEntrevista){
-            
-            quantEntrevistados++
-            
-
             if(j==0){
-
-                console.log("Entrevista #"+quantEntrevistados+" - "+ i+":"+j+"0 - "+listaEntrevistado[quantEntrevistados -1 ])
+                console.log("Entrevista #"+quantEntrevistados+" - ",i,":",j,"0 - "+listaEntrevistado[quantEntrevistados ])
             }else{
-                console.log("Entrevista #"+quantEntrevistados+" - "+ i+":"+j+" - "+listaEntrevistado[quantEntrevistados -1 ])
-
+                console.log("Entrevista #"+quantEntrevistados+" - "+ i+":"+j+" - "+listaEntrevistado[quantEntrevistados ])
             }   
-
+            quantEntrevistados++
         }
     }
 
-    console.log("********************************************************************************")
+console.log("************************************************************************************")
+var hora = 9
+var minutos = 15
+var total_entrevistas = 0
+const saida = 17
+entrevistados = [
+    "José da Silva",
+    "Antônio de Sá",
+    "Felipe Augusto",
+    "Carla Moreira",
+    "Pedro Malta",
+    "Maria Sousa",
+    "Marta da Silva",
+    "Fausto Augusto",
+    "Silvio Sávio",
+    "Maísa Silva",
+    "Lucas Lopes",
+    "Zenildo Santos",
+    "Bruno Lucas",
+    "Luana Melo",
+    "Felipe Santos",
+    "Flávio Miguel",
+    "Lauro Maria",
+    "Juca dos Santos",
+    "Luciana Carla",
+    "Felipe Silva",
+    "André Manuel",
+    "Pedro Parker",
+    "Ana Maria",
+    "Thiago Melo"
+]
+entrevistados.sort()
+for(hora; hora < saida; hora++){
+    if((hora == 12)||(hora==13)){
+        continue
+    }
+    for(min = 0; min < 60; min = min+minutos){
+        total_entrevistas++; 
+        if(min == 0){
+            console.log(hora+":"+min+"0","- Entrevista de", entrevistados[total_entrevistas]) 
+        }
+        else{
+            console.log(hora,":",min," - Entrevista de", entrevistados[total_entrevistas])
+    }
+}
+}
