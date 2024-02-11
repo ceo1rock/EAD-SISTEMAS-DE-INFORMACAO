0097c2b384d196bf7efc09063405d2486ec495ad
Augusto Rock <augusto.roque89@gmail.com>
2024-02-10 21:59:06 -0300

    canvas
    desenhando no canvas

---
+++ 1º SEMESTRE/01 - DESENVOLVIMENTO EM JAVASCRIPT/CONTEÚDO 02 - APIs de Navegador/02.03 - script.js
@@ -0,0 +1,9 @@
+
+// seleciona o elemento do canvas
+var canvas = document.getElementById('desenho');
+ 
+// variável de contexto 2d
+var ctx = canvas.getContext('2d');
+ 
+// desenha um retângulo
+ctx.fillRect(20, 20, 100, 100)
